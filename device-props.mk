8f6f5d996fd08a71c2a2bae5d573100a86e4ff7a
1nfrag <sil3ntcll@gmail.com>
2018-10-04 11:33:26 +0800

    dipper: Update camera & display props from stock
    Change-Id: Idc882403e49e28c8fc93908bfa9f8e795f52135c

--- device-props.mk
+++ device-props.mk
@@ -2,8 +2,13 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.sat.fallback.dist=45 \
     persist.camera.sat.fallback.dist.d=5 \
-    persist.camera.sat.fallback.luxindex=405 \
-    persist.camera.sat.fallback.lux.d=20
+    persist.camera.sat.fallback.luxindex=310 \
+    persist.camera.sat.fallback.lux.d=50 \
+    persist.vendor.camera.multicam.hwsync=TRUE \
+    persist.vendor.camera.multicam.fpsmatch=TRUE \
+    persist.vendor.camera.enableAdvanceFeatures=0x147 \
+    persist.vendor.camera.multicam.framesync=1 \
+    persist.camera.saf.stablecnt=3
 
 # Display features
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -14,4 +19,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qcom.ad.calib.data=/vendor/etc/sdr_config.cfg \
     ro.qcom.ad.hdr.calib.data=/vendor/etc/hdr_config.cfg \
     ro.qcom.ad.sensortype=2 \
-    ro.qualcomm.cabl=0
+    ro.qualcomm.cabl=0 \
+    ro.sf.lcd_density=440 \
+    ro.display.type=oled
