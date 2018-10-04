7fc9279b58bc0f6018d9e894b0342fd568500cf9
1nfrag <sil3ntcll@gmail.com>
2018-10-04 11:33:26 +0800

    dipper: Update camera & display props from stock
    Change-Id: Idc882403e49e28c8fc93908bfa9f8e795f52135c

--- device-props.mk
+++ device-props.mk
@@ -2,8 +2,9 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.sat.fallback.dist=45 \
     persist.camera.sat.fallback.dist.d=5 \
-    persist.camera.sat.fallback.luxindex=405 \
-    persist.camera.sat.fallback.lux.d=20
+    persist.camera.sat.fallback.luxindex=310 \
+    persist.camera.sat.fallback.lux.d=50 \
+    persist.camera.saf.stablecnt=3
 
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
