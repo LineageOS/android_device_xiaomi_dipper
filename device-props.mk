21caa12765cdae840bb85ec9b557553929b4ea87
TheScarastic <warabhishek@gmail.com>
2018-11-02 16:17:49 -0400

    dipper: Remove display post-processing properties
     * Used ones were moved to the common tree and the rest to the recycle bin.
    
    Change-Id: Ifc73e5bb73fab83fa6a46eb5fdd8748066e58255

--- device-props.mk
+++ device-props.mk
@@ -4,14 +4,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.sat.fallback.dist.d=5 \
     persist.camera.sat.fallback.luxindex=405 \
     persist.camera.sat.fallback.lux.d=20
-
-# Display features
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.displayfeature.histogram.enable=true \
-    ro.eyecare.brightness.threshold=3 \
-    ro.eyecare.brightness.level=8 \
-    ro.qcom.ad=1 \
-    ro.qcom.ad.calib.data=/vendor/etc/sdr_config.cfg \
-    ro.qcom.ad.hdr.calib.data=/vendor/etc/hdr_config.cfg \
-    ro.qcom.ad.sensortype=2 \
-    ro.qualcomm.cabl=0
