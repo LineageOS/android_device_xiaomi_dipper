7a07ce503ba9c51d26ab54cb0a236d5f6aea2f18
Bruno Martins <bgcngm@gmail.com>
2019-01-12 11:34:58 +0000

    dipper: Set display density
    Change-Id: Ie35343e261ecf8717ac88644a401576f1f5818ac

--- device-props.mk
+++ device-props.mk
@@ -5,6 +5,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.sat.fallback.luxindex=405 \
     persist.camera.sat.fallback.lux.d=20
 
+# Display density
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=440
+
 # Display features
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.displayfeature.histogram.enable=true \
