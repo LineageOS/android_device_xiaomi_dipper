e9b480163564ff120d2d72cfb5cd28ced6cbe307
Bruno Martins <bgcngm@gmail.com>
2019-02-23 22:58:25 +0000

    dipper: Append device manifest to common one
     * No more hacks needed.
    
    Change-Id: Ie5798c2223752eb9a73204eced65aacb29eb8b68

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,11 +7,3 @@ LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := etc/fstab.qcom
 LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.target.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.target.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
-include $(BUILD_PREBUILT)
