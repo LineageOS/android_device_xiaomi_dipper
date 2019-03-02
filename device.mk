a8084d33d6074b1ecec05d2864f136165ca56350
1nfrag <sil3ntcll@gmail.com>
2019-03-02 10:31:18 +0800

    dipper: Set up NFC
    Change-Id: Ib10e931059ad185e26541ceb68390124bb64686e

--- device.mk
+++ device.mk
@@ -67,13 +67,18 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl \
     com.android.nfc_extras \
     com.nxp.nfc.nq \
+    com.nxp.nfc.nq.xml \
+    libnqnfc-nci \
+    libnqp61-jcop-kit \
+    nfc_nci.nqx.default \
     nqnfcee_access.xml \
     nqnfcse_access.xml \
     NQNfcNci \
-    SecureElement \
-    Tag
+    Tag \
+    vendor.nxp.hardware.nfc@1.1-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
@@ -82,3 +87,7 @@ PRODUCT_COPY_FILES += \
 # PocketMode
 PRODUCT_PACKAGES += \
     XiaomiPocketMode
+
+# Secure element
+PRODUCT_PACKAGES += \
+    SecureElement
