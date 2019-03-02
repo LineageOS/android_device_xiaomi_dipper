29f5ce9db71dd88103b99616eca16541347d019a
1nfrag <sil3ntcll@gmail.com>
2019-03-02 10:31:18 +0800

    dipper: Set up NFC
    Change-Id: Ib10e931059ad185e26541ceb68390124bb64686e

--- device.mk
+++ device.mk
@@ -67,18 +67,29 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl \
     com.android.nfc_extras \
     com.nxp.nfc.nq \
+    com.nxp.nfc.nq.xml \
+    libnqnfc-nci \
+    libnqp61-jcop-kit \
+    nfc_nci.nqx.default \
     nqnfcee_access.xml \
     nqnfcse_access.xml \
     NQNfcNci \
-    SecureElement \
-    Tag
+    Tag \
+    vendor.nxp.hardware.nfc@1.1-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/nfc/libese-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libese-nxp.conf
 
 # PocketMode
 PRODUCT_PACKAGES += \
     XiaomiPocketMode
+
+# Secure element
+PRODUCT_PACKAGES += \
+    SecureElement \
+    android.hardware.secure_element@1.0-service
