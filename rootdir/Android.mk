265fb67bf97328be476e83fab6353d5f07689cc2
1nfrag <sil3ntcll@gmail.com>
2019-03-02 13:30:30 +0800

    dipper: add init for gps & camera
    Change-Id: I8127a758f2b9d2d1487030bde7523a33d74c81e1

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,3 +7,19 @@ LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := etc/fstab.qcom
 LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := init.brcm.rc
+LOCAL_MODULE_TAGS   := optional eng
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := etc/init.brcm.rc
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := init.remosaic.rc
+LOCAL_MODULE_TAGS   := optional eng
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := etc/init.remosaic.rc
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
