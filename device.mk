3c728f0b56be571f5f201656c45026fba0243c9c
Bruno Martins <bgcngm@gmail.com>
2019-03-07 21:31:08 +0000

    dipper: Align NFC configs with upstream
     * Match all the changes up to LA.UM.7.3.r1-06900-sdm845.0
    
    Change-Id: I9d1ace9fdc102e6cc7f9fee9cd0a10be027dd90e

--- device.mk
+++ device.mk
@@ -66,7 +66,7 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Permissions
