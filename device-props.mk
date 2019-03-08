375eaf15a864b6680b74aaa782d9f13839b2856a
1nfrag <sil3ntcll@gmail.com>
2019-03-08 15:28:12 +0800

    dipper: Set device stock props
    Change-Id: I3107a4c760f56945f6e483d2e361ffcff2a764e9

--- device-props.mk
+++ device-props.mk
@@ -1,3 +1,7 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.audio.soundfx.type=mi
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.sat.fallback.dist=45 \
@@ -13,4 +17,24 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.displayfeature.histogram.enable=true \
     ro.eyecare.brightness.threshold=3 \
-    ro.eyecare.brightness.level=8
+    ro.eyecare.brightness.level=8 \
+    ro.hist.brightness.threshold=5
+
+# Display postprocessing
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.display.enable_default_color_mode=0
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.fp.fpc=true \
+    ro.hardware.fp.goodix=true
+
+# GNSS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.flp=brcm \
+    ro.hardware.gps=brcm
+
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.nfc_nci=nqx.default \
+    ro.se.type=HCE,UICC
