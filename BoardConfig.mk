b9267cb7be8cff9ceaef78ddf783861c941e16cf
Giuseppe Barillari <joe2k01dev@gmail.com>
2019-03-08 19:25:32 +0100

    dipper: Address gps denials
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>
    Change-Id: I49c189318c75f66a8de86759ce7bdb8586fc94ee

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,3 +38,4 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
