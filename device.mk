de55c5ca4d255cef8bc6e0024a28e4c638881298
Bruno Martins <bgcngm@gmail.com>
2019-03-13 21:00:33 +0000

    dipper: Import stock NFC configs
     * Configs taken from MIUI 9.3.7 global dev build
    
    Change-Id: Ie8149f4990fa85c0bb9ad7f6338c646d50b4d33b

--- device.mk
+++ device.mk
@@ -55,3 +55,7 @@ PRODUCT_PACKAGES += \
     NQNfcNci \
     SecureElement \
     Tag
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
