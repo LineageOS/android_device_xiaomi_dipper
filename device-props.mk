bdfdaac73aa48927b3fc30a3b3601e19fe4a6f93
Bruno Martins <bgcngm@gmail.com>
2019-08-02 09:33:37 +0100

    dipper: Kill Xiaomi's display feature
     * This damn HAL was breaking night light and who knows what else.
    
     * Remove all its related props as those are useless without
       the service running.
    
    Change-Id: Ia1a9ba58e74129bc1cac0f0a0fe863d3057750e2

--- device-props.mk
+++ device-props.mk
@@ -13,13 +13,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=440
 
-# Display features
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.displayfeature.histogram.enable=true \
-    ro.eyecare.brightness.threshold=3 \
-    ro.eyecare.brightness.level=8 \
-    ro.hist.brightness.threshold=5
-
 # Display postprocessing
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_default_color_mode=0
