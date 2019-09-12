8df96ed6dd3f27203920ad75ca4abeed275c9284
LuK1337 <priv.luk@gmail.com>
2019-09-12 23:57:51 +0200

    dipper: Stop overriding TARGET_DEVICE
    Not only TARGET_DEVICE is no longer used for generating props
    (as tools/buildinfo_common.sh is using PRODUCT_DEVICE instead),
    but also there's no need to override PRODUCT_DEVICE because
    it is already set to the desired value.
    
    Change-Id: I16bd7b5bfaab5225478921f2b532c40a99eda972

--- lineage_dipper.mk
+++ lineage_dipper.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -20,7 +20,6 @@ BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIF
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys" \
-    PRODUCT_NAME="dipper" \
-    TARGET_DEVICE="dipper"
+    PRODUCT_NAME="dipper"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
