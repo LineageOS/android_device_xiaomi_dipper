7c2ccc355e7483bec62b687e3b7ff2f5588cded4
Bruno Martins <bgcngm@gmail.com>
2019-09-29 14:22:58 +0100

    dipper: Convert wide color display configstore setting to a prop
    Surfaceflinger now uses the system properties instead of configstore.
    
    Change-Id: If5b7542d9d00a819a64fe32234c197a26e9fc388

--- device-props.mk
+++ device-props.mk
@@ -9,6 +9,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.sat.fallback.luxindex=405 \
     persist.camera.sat.fallback.lux.d=20
 
+# Display
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.has_wide_color_display=true
+
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=440
