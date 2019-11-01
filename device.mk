b065854ecbabbff24ebc422f80a20115352f13a6
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    dipper: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
