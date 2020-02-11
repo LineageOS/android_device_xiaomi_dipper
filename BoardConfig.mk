83ae8381cd78f7c44571c595e973b10d415670c5
Bruno Martins <bgcngm@gmail.com>
2020-02-11 21:56:09 +0000

    Revert "dipper: Set face unlock camera ID"
    This reverts commit 30e81858680783a42014c6603112fdc4f9e1f4bc.
    
    Reason for revert: Google removed Trusted face feature in 10.
    
    Change-Id: I218dd0f8e9c66f28ce5c09bb3cd1c684a2b26a9f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,9 +12,6 @@ DEVICE_PATH := device/xiaomi/dipper
 # Assert
 TARGET_OTA_ASSERT_DEVICE := dipper
 
-# Camera
-TARGET_FACE_UNLOCK_CAMERA_ID := 5
-
 # Kernel
 TARGET_KERNEL_CONFIG := dipper_defconfig
 
