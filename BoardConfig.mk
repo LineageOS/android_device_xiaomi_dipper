928bc2ff64942067b0a82b826f2858379e66552d
Bruno Martins <bgcngm@gmail.com>
2020-02-18 23:49:20 +0000

    dipper: Split system and vendor props
    Change-Id: If965be42e68e9ebbd816656dd2af79f541c1ceea

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,10 @@ TARGET_USES_NQ_NFC := true
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
--- device.mk
+++ device.mk
@@ -37,9 +37,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-# Properties
--include $(LOCAL_PATH)/device-props.mk
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
