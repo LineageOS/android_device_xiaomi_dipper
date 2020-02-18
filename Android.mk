5c4c7143b41eff87a7511384aaef07996ea5dff2
1nfrag <sil3ntcll@gmail.com>
2020-02-18 15:22:18 +0800

    dipper: Symlink gps.conf from system to vendor
    Silences the warning: Couldn't open file '/etc/gps.conf' for read
    
    Change-Id: If83d588e0c11a8b4bf3b3d91aafe633cacaade3c

--- Android.mk
+++ Android.mk
@@ -18,4 +18,15 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),dipper)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+include $(CLEAR_VARS)
+
+
+GPS_CONF_SYMLINK := $(PRODUCT_OUT)/system/etc/gps.conf
+$(GPS_CONF_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf /vendor/etc/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GPS_CONF_SYMLINK)
+
 endif
