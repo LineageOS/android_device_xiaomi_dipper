189d69cb640fd3d0c43ce8b9b10d53aa3b64e66b
Bruno Martins <bgcngm@gmail.com>
2020-05-17 11:53:24 +0100

    dipper: Switch to OSS fingerprint HIDL
    Change-Id: I2abf0e136b31c64f827bac99f391d86a8c9e1fd9

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2018-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -64,6 +64,10 @@ PRODUCT_PACKAGES += \
 # Device init scripts
 PRODUCT_PACKAGES += \
     init.target.rc
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm845
 
 # GPS
 PRODUCT_COPY_FILES += \
