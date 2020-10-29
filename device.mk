505484319d22a558b26861124754a66757431ee4
LuK1337 <priv.luk@gmail.com>
2020-10-29 14:31:02 +0100

    Revert "dipper: Exclude lineage-sdk overlays from RRO"
    This reverts commit c9bfe84d4b904c54120bb85f610b06327a1e5e11.
    
    Change-Id: I12f150b13686f8b91bb63e4ee728eb4016856aa5

--- device.mk
+++ device.mk
@@ -21,9 +21,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
-
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
