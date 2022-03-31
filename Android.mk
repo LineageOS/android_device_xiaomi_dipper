e4a7cf13def84265352d5a086f87ec9e47b675d1
LuK1337 <priv.luk@gmail.com>
2022-03-31 18:05:37 +0200

    Revert "dipper: Make Android.mk search recursive"
    No longer needed.
    
    This reverts commit f3b74a919afebcaaa305ce85d2d1525fd6aab8e9.
    
    Change-Id: Iae9c4257a98dd4ba47ef9992b1dec133ccc56764

--- Android.mk
+++ Android.mk
@@ -16,8 +16,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),dipper)
-  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
