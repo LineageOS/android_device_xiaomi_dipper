274ff6accba3c7da6265f98ca97710722ed6de9b
Bruno Martins <bgcngm@gmail.com>
2025-03-29 12:51:31 +0000

    dipper: Setup ELF checks
    Change-Id: Ida6d2d91ed0a722b9cfadd71b6221dac614c33ca

--- extract-files.py
+++ extract-files.py
@@ -8,26 +8,40 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
+from extract_utils.fixups_lib import (
+    lib_fixups,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
 
 namespace_imports = [
+    'hardware/qcom-caf/sdm845',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/display',
     'vendor/xiaomi/sdm845-common'
 ]
 
 blob_fixups: blob_fixups_user_type = {
     'vendor/lib/camera/components/com.qti.node.watermark.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
+    (
+        'vendor/lib/libarcsoft_dualcam_refocus_front.so',
+        'vendor/lib/libarcsoft_dualcam_refocus_rear_t.so',
+        'vendor/lib/libarcsoft_dualcam_refocus_rear_w.so'
+    ): blob_fixup()
+        .clear_symbol_version('remote_handle_close')
+        .clear_symbol_version('remote_handle_invoke')
+        .clear_symbol_version('remote_handle_open'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
     'dipper',
     'xiaomi',
-    namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
-    check_elf=False,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
